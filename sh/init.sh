34eead39110abba070cf2b5e7645a1e0325f5158
Ryan Bucshon <busheezy@gmail.com>
2019-01-21 22:51:07 -0600

    dumb

--- sh/init.sh
+++ sh/init.sh
@@ -3,7 +3,7 @@ SOURCEMOD_VERSION=1.9
 SOURCEMOD_VERSION_FULL=1.9.0
 SOURCEMOD_BUILD=6274
 
-CSGO_PATH="${sb_serverPath}/csgo"
+CSGO_PATH="${bp_serverPath}/csgo"
 echo "${CSGO_PATH}"
 echo "Downloading sourcemod"
 wget -nv https://sm.alliedmods.net/smdrop/${SOURCEMOD_VERSION}/sourcemod-${SOURCEMOD_VERSION_FULL}-git${SOURCEMOD_BUILD}-linux.tar.gz
