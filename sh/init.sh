35e11c4c768dc6653c2cb16d76dcfab2733b276d
Ryan Bucshon <busheezy@gmail.com>
2019-10-13 13:21:05 -0500

    mean servers

--- sh/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SOURCEMOD_VERSION=1.9
-SOURCEMOD_VERSION_FULL=1.9.0
-SOURCEMOD_BUILD=6281
-FILE_NAME="sourcemod-${SOURCEMOD_VERSION_FULL}-git${SOURCEMOD_BUILD}-linux.tar.gz"
-wget -q -nv https://sm.alliedmods.net/smdrop/${SOURCEMOD_VERSION}/${FILE_NAME}
-tar -xf sourcemod-${SOURCEMOD_VERSION_FULL}-git${SOURCEMOD_BUILD}-linux.tar.gz -C ${bp_modDir}
-rm sourcemod-${SOURCEMOD_VERSION_FULL}-git${SOURCEMOD_BUILD}-linux.tar.gz
